a4cd9d27453dc4ec91fead88d0a3f16ad42a4057
jcarlos <jcori.transoft@gmail.com>
2019-04-05 18:42:28 -0400

    ui for device

--- bootui/src/app/app.module.ts
+++ bootui/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GridComponent } from './components/grid/grid.component';
 import { GridWComponent } from './components/grid-w/grid-w.component';
+import { DestinationComponent } from './components/destination/destination.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GridComponent,
-    GridWComponent
+    GridWComponent,
+    DestinationComponent
   ],
   imports: [
     BrowserModule,
