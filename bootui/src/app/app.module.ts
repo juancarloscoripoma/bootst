241a2a6fe6b154806842127eac4337823aaa227a
jcarlos <jcori.transoft@gmail.com>
2019-04-05 19:27:25 -0400

    iu for outputs

--- bootui/src/app/app.module.ts
+++ bootui/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { GridComponent } from './components/grid/grid.component';
 import { GridWComponent } from './components/grid-w/grid-w.component';
 import { DestinationComponent } from './components/destination/destination.component';
+import { OutputsComponent } from './components/outputs/outputs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GridComponent,
     GridWComponent,
-    DestinationComponent
+    DestinationComponent,
+    OutputsComponent
   ],
   imports: [
     BrowserModule,
